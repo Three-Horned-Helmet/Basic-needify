a8ff3f1988fe6892f5c481bdd2b04de2e4e95961
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-12 20:16:09 +0200

    added navigation bar

---
+++ components/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>IT WORKS</Text>
+    </View>
+  );
+};
+
+export default Home;
