2247b69e945fb9dde624732b9f91b4627f18f668
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-12 20:29:32 +0200

    finished the project

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,12 +1,43 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>IT WORKS</Text>
-    </View>
-  );
+  const [chore, setChore] = useState({
+    drink: false,
+    sleep: false,
+    air: false,
+  });
+
+  const handlePress = (key) => {
+    setChore({
+      ...chore,
+      [key]: !chore[key],
+    });
+  };
+
+  const renderChores = [];
+  for (let key in chore) {
+    renderChores.push(
+      <TouchableOpacity
+        key={key}
+        onPress={() => {
+          handlePress(key);
+        }}
+      >
+        <Text style={chore[key] ? styles.done : styles.needToDo}>{key}</Text>
+      </TouchableOpacity>
+    );
+  }
+  return <View>{renderChores}</View>;
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  needToDo: {
+    color: "red",
+  },
+  done: {
+    color: "green",
+  },
+});
